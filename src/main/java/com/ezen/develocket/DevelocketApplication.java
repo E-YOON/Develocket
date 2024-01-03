7f09b8e5c841400f08bd4508194c663ac7457f73
E-YOON <yunjeongy930@gmail.com>
2024-01-03 20:59:18 +0900

    first commit

---
+++ src/main/java/com/ezen/develocket/DevelocketApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.develocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelocketApplication.class, args);
+	}
+
+}
